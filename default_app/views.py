19713b30d9823912d9d46cb66a465504bf2228a1
Alexey Yefimenko <alesha95@ukr.net>
2017-07-01 19:45:07 +0300

    home template is modified

--- default_app/views.py
+++ default_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+
 #from django.http import HttpResponse
 # Create your views here.
+from catalog.models import Catalog
+
 
 def home(request):
     #return HttpResponse('This is home page')
     context = {
-        'title' : 'Home title'
+        'title':'Home title',
+        'catalog' : Catalog.objects.all(),
     }
     return render(request, 'default_app/home.html', context)
 
