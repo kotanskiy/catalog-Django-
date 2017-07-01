c360249243ac38b6e78cdb77961bdb03205031e1
Alexey Yefimenko <alesha95@ukr.net>
2017-07-01 19:55:06 +0300

    refactoring

--- default_app/views.py
+++ default_app/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-
-#from django.http import HttpResponse
-# Create your views here.
 from catalog.models import Catalog
-
+# Create your views here.
 
 def home(request):
-    #return HttpResponse('This is home page')
     context = {
         'title':'Home title',
         'catalog' : Catalog.objects.all(),
